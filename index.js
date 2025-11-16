6a8b8937f979fb0ac3e8a04e3f4944caf549384b
Souvik Biswas <biswassouvik284@gmail.com>
2025-11-16 13:51:09 +0530

    Redirect Short URL

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const connectMongoDB = require("./connect");
 const urlRouter = require("./routes/url");
+const URL = require("./models/url");
 
+// Global Variables
 const app = express();
 const PORT = 8001;
 const dbUrl = "mongodb://localhost:27017";
 const dbName = "short-url";
 
+// Connect to Database
 connectMongoDB(dbUrl, dbName)
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => `Cannot connect to DB. Error: ${error}`);
 
+// to parse req body (json)
 app.use(express.json());
 
 app.use("/url", urlRouter);
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    { shortId },
+    { $push: { visitHistory: { timestamp: Date.now() } } }
+  );
+  return res.redirect(entry.redirectUrl);
+});
 
 app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
